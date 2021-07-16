4bf8f897d5897acb795d495355c13a888f11beea
diegolins10 <diegofernandeslins@gmail.com>
2021-07-15 21:10:09 -0300

    super class employee

---
+++ Payment/Entities/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Payment.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHour { get; set; }
+
+        public Employee()
+        {
+
+        }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+        public double Payment()
+        {
+            return ValuePerHour * Hours;
+        }
+    }
+
+    
+}
