62f14b31a311063500ea1b23a5d4f931d73e4a2c
diegolins10 <diegofernandeslins@gmail.com>
2021-07-15 21:19:23 -0300

    sub class fun terceirizado

---
+++ Payment/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Payment.Entities
+{
+    class OutsourcedEmployee : Employee
+    {
+        public double additionalCharge  { get; set; }
+
+        public OutsourcedEmployee()
+        {
+
+        }
+        // reaproveitando construtor da super classe
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge) : base (name, hours, valuePerHour)
+        {
+            this.additionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + (1.1 * additionalCharge);
+        }
+    }
+}
